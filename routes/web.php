7634cda2304496fe5f18fe6b318c4282c91c7642
ristianaditya <ristianaditya35@gmail.com>
2020-06-11 21:07:45 +0700

    refactor(home, about): ubah method Pagescontroller

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', 'PagesController@home');
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', 'PagesController@about');
