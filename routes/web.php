4f3feca9bed79b85a914d9b4cf15823cd9a3a907
ristianaditya <ristianaditya35@gmail.com>
2020-06-12 19:26:12 +0700

    feat<softdelete>:menambahkan softdelete di student

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,15 @@ Route::get('/about', 'PagesController@about');
 
 Route::get('/siswa', 'SiswaController@index');
 
-Route::get('/semuasiswa', 'StudentsController@index');
-Route::get('/students/{student}', 'StudentsController@show');
-Route::delete('/students/{student}', 'StudentsController@destroy');
-Route::get('/students/{student}/edit', 'StudentsController@edit');
-Route::get('/tambah', 'StudentsController@create');
-Route::post('/create', 'StudentsController@store');
-Route::patch('/students/{student}', 'StudentsController@update');
+// Route::get('/semuasiswa', 'StudentsController@index');
+// Route::get('/students/{student}', 'StudentsController@show');
+// Route::delete('/students/{student}', 'StudentsController@destroy');
+// Route::get('/students/{student}/edit', 'StudentsController@edit');
+// Route::get('/tambah', 'StudentsController@create');
+// Route::post('/create', 'StudentsController@store');
+// Route::patch('/students/{student}', 'StudentsController@update');
+
+Route::resource('students', 'StudentsController');
+Route::resource('tambah', 'StudentsController');
+Route::resource('create', 'StudentsController');
+Route::resource('semuasiswa', 'StudentsController');
